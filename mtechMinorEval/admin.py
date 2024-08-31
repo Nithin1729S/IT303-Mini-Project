41615be26a1d0a52bf957d7e5f530d42a6c6e3bb
Nithin1729S <sureshnithin1729@gmail.com>
2024-09-01 01:04:30 +0530

    user profile register bug

--- mtechMinorEval/admin.py
+++ mtechMinorEval/admin.py
@@ -3,8 +3,5 @@ from django.contrib import admin
 # Register your models here.
 from .models import Project,Profile,Student,Faculty,ExaminerEvaluation,GuideEvaluation
 admin.site.register(Project)
-admin.site.register(Profile)
-admin.site.register(Student)
-admin.site.register(Faculty)
 admin.site.register(ExaminerEvaluation)
 admin.site.register(GuideEvaluation)
