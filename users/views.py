f07c8c018b9089bb42759398fc00c60f292cb23d
Nithin1729S <sureshnithin1729@gmail.com>
2024-09-01 06:16:40 +0530

    restricted login only to faculty

--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib import messages
+from users.models import Profile
 
 def loginUser(request):
     if request.user.is_authenticated:
@@ -13,15 +14,22 @@ def loginUser(request):
 
         try:
             user=User.objects.get(username=username)
-        except:
+        except User.DoesNotExist:
             messages.error(request,"Username does not exist")
 
         user=authenticate(request,username=username,password=password)
         if user is not None:
-            login(request,user)
-            return redirect('projectsList')
+            try:
+                profile = Profile.objects.get(user=user)
+                if profile.role == 'faculty':
+                    login(request, user)
+                    return redirect('projectsList')
+                else:
+                    messages.error(request, "You do not have permission to access this area")
+            except Profile.DoesNotExist:
+                messages.error(request, "Profile not found for the user")
         else:
-            messages.error(request,"Username or password is incorrect")
+            messages.error(request, "Username or password is incorrect")
 
     return render(request,'users/login.html')
 
