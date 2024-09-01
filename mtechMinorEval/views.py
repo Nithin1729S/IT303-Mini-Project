ceb833d06d9b14cf2f329ccee08c9766d4496ef0
Nithin1729S <sureshnithin1729@gmail.com>
2024-09-01 06:47:04 +0530

    project list display to only guides and examiners

--- mtechMinorEval/views.py
+++ mtechMinorEval/views.py
@@ -1,22 +1,34 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 # Create your views here.
-from .models import Project
+from .models import *
 from .forms import ExaminerEvaluationForm, GuideEvaluationForm
+from users.models import *
 
 def home(request):
     return render(request,'mtechMinorEval/home.html')
 
 @login_required(login_url='login')
 def projectsList(request):
-    projects=Project.objects.all()
-    context={'projects':projects}
-    return render(request,'mtechMinorEval/projectsList.html',context=context)
+    user = request.user
+    userEmail = user.email
+    userProfile = get_object_or_404(Profile, email=userEmail)
+    projects = []
+    if userProfile.role == 'faculty':
+        faculty = get_object_or_404(Faculty, profile=userProfile)
+        guide_projects = Project.objects.filter(guide=faculty)
+        examiner_projects = Project.objects.filter(examiner=faculty)
+        projects = guide_projects.union(examiner_projects)
+    else:
+        logout(request,user)
+    context = {'projects': projects}
+    return render(request, 'mtechMinorEval/projectsList.html', context=context)
 
 @login_required(login_url='login')
 def evaluate(request):
     formExaminer=ExaminerEvaluationForm()
-    formGuide=GuideEvaluationForm
+    formGuide=GuideEvaluationForm()
     context={'formExaminer':formExaminer ,'formGuide':formGuide}
     return render(request,'mtechMinorEval/projectEvaluation.html',context=context)
