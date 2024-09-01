a8c1e0ab8a31ae426594da2faad869aa64d4da94
Nithin1729S <sureshnithin1729@gmail.com>
2024-09-01 09:10:04 +0530

    url issue fix

--- mtechMinorEval/urls.py
+++ mtechMinorEval/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns=[
     path('',views.home,name='home'),
-    path('projectsList',views.projectsList,name='projectsList'),
+    path('projectsList/',views.projectsList,name='projectsList'),
     path('evaluate/<str:pk>/',views.evaluate,name='evaluate')
 ]
