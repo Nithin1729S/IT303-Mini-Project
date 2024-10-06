69dc9e82973b896020fddb7b26369a6bb9ef73d9
Nithin1729S <sureshnithin1729@gmail.com>
2024-10-06 09:52:43 +0530

    updated requirements

--- it303/settings.py
+++ it303/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'mtechMinorEval.apps.MtechminorevalConfig',
     'users.apps.UsersConfig'
 ]
